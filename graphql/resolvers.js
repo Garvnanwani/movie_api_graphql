1feb96980fd3260733c2c695f7265d4c93b29f73
Garv <garvwashere@gmail.com>
2021-04-22 20:32:29 +0530

    rest api wrapped to graphql

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,11 @@
-import { getById, people } from './db.js'
+import { getMovie, getMovies, getSuggestions } from "./db.js";
 
 const resolvers = {
-    Query: {
-        people: () => people,
-        person: (_, {id}) => getById(id)
-    },
-}
+  Query: {
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
+  }
+};
 
-export default resolvers
+export default resolvers;
